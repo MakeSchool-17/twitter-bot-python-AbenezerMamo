d539a9db171045b9aa51b33f34faf99e15b26ba7
AbenezerMamo <ElitePowered@gmail.com>
2015-09-24 14:22:11 -0700

    added support for command line arguments for dictionary_words. You can start the script as python3 dictionary_words.py 12. Which in turn would return 12 words from the dictionary in random order.

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,9 +1,11 @@
 import random
+from sys import argv
+
 content = open('/usr/share/dict/words').read().split()
 words_list = []
 for words in content:
     words_list.append(words)
 sentence = []
-while len(sentence) < 5:
+while len(sentence) < int(argv[1]):
     sentence.append(words_list[random.randint(0, len(words_list))])
 print(*sentence)
