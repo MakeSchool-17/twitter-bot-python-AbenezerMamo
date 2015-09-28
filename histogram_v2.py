5819c978dd7d20e5a453dfe1284d54319f81397c
AbenezerMamo <ElitePowered@gmail.com>
2015-09-28 11:56:09 -0700

    Added stochastic script and optimized code

--- histogram_v2.py
+++ histogram_v2.py
@@ -9,6 +9,7 @@ histo = []
 source_text_filtered = re.sub(r'\W+', ' ', source_text)
 def histogram(source_text_filtered):
     found_at = None
+    found = None
     for word in source_text_filtered:
         for index, (histo_word, freq) in enumerate(histo):
             if word == histo_word:
@@ -18,7 +19,6 @@ def histogram(source_text_filtered):
             histo.append((word, 1))
         else:  # repeat occurence
             histo[found_at][1] += 1
-
 def unique_words(histogram):
     return len(histo)
 
--- sample.py
+++ sample.py
@@ -1,10 +1,11 @@
 import re
 import string
 
-source_text = open('book.txt').read().split()
+source_text = open('sample.txt').read().split()
 
 
 histo = {}
+histo_new = {}
 def histogram(source_text):
     for word in source_text:
         if word not in histo:
@@ -17,13 +18,18 @@ def histogram(source_text):
             new_word = new_word.lstrip('_')
             new_word = new_word.rstrip('_')
             histo[new_word] += 1
-    return histo
+        for word in histo:
+            histo_new[word] /= len(source_text)
 
-def unique_words(histogram):
-    return len(histo)
 
-def frequency(word, histogram):
-    return histo[word]
+def random_sentence():
+    histogram(source_text)
+    for key, value in histo_new:
+        if value >= 0:
+            sentence.append(key)
+    return sentence
+
+
 
 if __name__ == '__main__':
-    print(histogram(source_text))
+    print(random_sentence())
