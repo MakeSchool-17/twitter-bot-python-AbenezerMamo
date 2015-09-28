91a4d72f12ab4e57941e2c149020a8fd9e8206e4
AbenezerMamo <ElitePowered@gmail.com>
2015-09-28 12:38:22 -0700

    generated random sentences for histo word values with probability

--- sample.py
+++ sample.py
@@ -1,26 +1,40 @@
 import re
 import string
+import random
+from sys import argv
+
+word_count = len(argv)
+sentence = []
+histo = {}
 
 source_text = open('sample.txt').read().split()
 def histogram(source_text):
-    histo = {}
-    histo_new = {}
     for word in source_text:
         if word not in histo:
-            new_word = re.sub(r"\W+", "", word)
-            new_word = new_word.lstrip('_')
-            new_word = new_word.rstrip('_')
-            histo[new_word] = 1
+            filter_text(word)
+            histo[word] = 1
         else:
-            new_word = re.sub(r"\W+", "", word)
-            new_word = new_word.lstrip('_')
-            new_word = new_word.rstrip('_')
-            histo[new_word] += 1
+            filter_text(word)
+            histo[word] += 1
     for key, value in histo.items():
         histo_new[key] = (value / len(source_text))
     return histo_new
 
+def filter_text(word):
+        word = re.sub(r"\W+", "", word)
+        word = word.lstrip('_')
+        word = word.rstrip('_')
+        return word
+
+def random_sentence():
+    histogram(source_text)
+    #histo_new = sorted(histo_new.items(), key=operator.itemgetter(1))
+    while counter < word_count:
+        for key, value in histo_new:
+            if value > (1 - (word_count / len(histo_new))):
+                sentence.append(key)
+    return sentence.items()
+
 
 if __name__ == '__main__':
-    print(histogram(source_text))
-    print(len(source_text))
+    print(random_sentence)
