489da2664122fc15aa259da9116668e685766c72
AbenezerMamo <ElitePowered@gmail.com>
2015-10-02 10:27:47 -0700

    initial setup for stack data structure

---
+++ stack.py
@@ -0,0 +1,25 @@
+class Stack:
+
+    def __init__(self):
+        pass
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+
+    def __str__(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+class Node:
+    def __init__(self):
+        pass
