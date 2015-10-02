e64ec3955d38b3b69496a5858d547e011f051bb6
AbenezerMamo <ElitePowered@gmail.com>
2015-10-02 10:53:09 -0700

    hash table setup

---
+++ hash_table.py
@@ -0,0 +1,20 @@
+import node
+
+class HashTable:
+    def __init__(self):
+        pass
+
+    def set(self):
+        pass
+
+    def get(self):
+        pass
+
+    def update(self):
+        pass
+
+    def get_keys(self):
+        pass
+
+    def get_values(self):
+        pass
