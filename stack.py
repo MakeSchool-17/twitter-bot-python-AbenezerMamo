48e0fd83f66fd959d5ba4f6d50e1900947696ec6
AbenezerMamo <ElitePowered@gmail.com>
2015-10-02 10:49:32 -0700

    added stack data structure

--- stack.py
+++ stack.py
@@ -1,24 +1,46 @@
 class Stack:
 
     def __init__(self):
-        pass
+        self.head = None
+        self.size = 0
+
 
     def push(self):
-        pass
+        new_node = Node()
+        new_node.data = data
+        next_node = self.head
+        self.head = new_node
+        self.head.next = next_node
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.is_empty():
+            return None
+        else:
+            first_element = self.head
+            self.head = first_element.next
+            self.size -= 1
+            return first_element.data
 
     def peek(self):
-        pass
+        if self.is_empty: # If this exists. You can also check if self.head actually exists
+            return None
+        else:
+            return self.head.data
 
     def size(self):
+        return self.size
 
     def __str__(self):
-        pass
+        return_str = ''
+        current_node = self.head
+        while current_node is not None:
+            return_str += str(current_node.data)
+            current_node = current_node.next
+        return return_str
 
     def is_empty(self):
-        pass
+        return self.head is None
 
 class Node:
     def __init__(self):
